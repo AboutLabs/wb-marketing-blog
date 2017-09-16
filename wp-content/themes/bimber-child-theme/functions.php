b82b866c0368f27678fd829fc00c1636518c342e
Simon Aumayer <simonaumayer@Simons-MBP.fritz.box>
2017-09-16 08:29:36 +0200

    included Source Sans Pro Google Font

--- wp-content/themes/bimber-child-theme/functions.php
+++ wp-content/themes/bimber-child-theme/functions.php
@@ -11,3 +11,13 @@ if ( !defined( 'ABSPATH' ) )
 */
 function bimber_child_setup() {}
 add_action( 'after_setup_theme', 'bimber_child_setup' );
+
+function google_fonts() {
+	$query_args = array(
+		'family' => 'Source+Sans+Pro:300,400',
+		'subset' => 'latin,latin-ext'
+	);
+	wp_register_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
+            }
+            
+add_action('wp_enqueue_scripts', 'google_fonts');
