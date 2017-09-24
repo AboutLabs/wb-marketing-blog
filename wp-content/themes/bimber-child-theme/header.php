3d7c3b2f58df4edf575ed8112754ea41e1f9fb0b
Simon Aumayer <simonaumayer@Simons-MBP.fritz.box>
2017-09-24 09:21:15 +0200

    added Google TM Code

--- wp-content/themes/bimber-child-theme/header.php
+++ wp-content/themes/bimber-child-theme/header.php
@@ -25,6 +25,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"/>
 
 	<?php wp_head(); ?>
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-PT8WSW8');</script>
+	<!-- End Google Tag Manager -->
+
 </head>
 
 <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
